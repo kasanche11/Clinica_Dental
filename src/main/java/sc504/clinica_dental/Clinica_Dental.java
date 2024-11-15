43870eefc153f016c29b0ff362771f6b850eddd6
kasanche11 <kevinsanchezseas@gmail.com>
2024-11-15 17:03:33 -0600

    1st Commit

---
+++ src/main/java/sc504/clinica_dental/Clinica_Dental.java
@@ -0,0 +1,9 @@
+package sc504.clinica_dental;
+
+public class Clinica_Dental {
+
+    public static void main(String[] args) {
+        Inicio ad = new Inicio();
+        ad.setVisible(true);
+    }
+}
